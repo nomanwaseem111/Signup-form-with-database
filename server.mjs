8dd23931f0f1fb2340e96bce4fe6d92304e999f3
Noman Waseem <nomanwaseem111@gmail.com>
2022-08-24 18:27:32 +0500

    final commit

--- server.mjs
+++ server.mjs
@@ -1,11 +1,23 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 const port = process.env.PORT || 3000
 
-let userBase = []
+const userSchema = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    email: String,
+    password: String,
+    createdOn: { type: Date, default: Date.now }
+});
+
+const userModel = mongoose.model('user', userSchema);
+
+
 
 app.post('/signup', (req, res) => {
 
@@ -27,36 +39,58 @@ app.post('/signup', (req, res) => {
     }
 
 
-    let isFound = false
-
-    for (let i = 0; i < userBase.length; i++) {
-        if (userBase[i].email === body.email.toLowerCase()) {
-            isFound = true
-            break;
-        }
-
-    }
-
-    if (isFound) {
-        res.status(401).send({message : "This Email is Already Registered"})
-        return;
-    }
 
 
-    let userName = {
+    let newUser = new userModel({
 
         fname: body.fname,
         lname: body.lname,
         email: body.email.toLowerCase(),
         fname: body.password,
-    }
+    })
+    newUser.save((err, result) => {
 
-    userBase.push(userName);
+        if (!err) {
+            console.log("Data Saved : ", result);
+            res.status(201).send({ message: "User is Created" })
+        } else {
+            console.log("db error : ", err);
+            res.status(500).send({ message: "internal Server Error" })
+        }
+    })
 
-    res.status(201).send({message : "User is Created"})
-    return
+
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+let dbURI = "mongodb+srv://abc:abc@cluster0.ko1vloy.mongodb.net/socialMediaBase?retryWrites=true&w=majority";
+// let dbURI = 'mongodb://localhost/mydatabase';
+mongoose.connect(dbURI);
+
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+    // process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
